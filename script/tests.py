1aac0ed19433bb4e689fdb2355eac6278e5ef93e
Andre <megabrean@gmail.com>
2022-04-07 22:49:28 -0300

    using hook from cached dpAr

--- script/tests.py
+++ script/tests.py
@@ -1,25 +1,19 @@
-from maya import cmds
+class Car():
+    def __init__(self, name) -> None:
+        self.name = name
 
-""" attrList = cmds.listAttr('Spine__dpAR_1:Guide_Base', userDefined=True)
-print(attrList)
-objAttrAndValues = {}
-objAttrAndValues['Spine__dpAR_1:Guide_Base'] = {}
-print(objAttrAndValues)
-for attr in attrList:
-    try:
-        objAttrAndValues['Spine__dpAR_1:Guide_Base'][attr] = cmds.getAttr('Spine__dpAR_1:Guide_Base.'+attr, silent=True)
-    except:
-        objAttrAndValues['Spine__dpAR_1:Guide_Base'][attr] = ''
+carroA = Car('Uno')
+carroB = Car('Gol')
+carroC = Car('Corsa')
+myDict = {}
+myList = [carroA, carroB, carroC]
 
-print(objAttrAndValues) """
-
-
-# print(cmds.listAttr(cmds.ls(selection=True), keyable=True))
-
-# [u'FkLine__dpAR_1:Guide_Base_RadiusCtrl', u'FkLine__dpAR_1:Guide_Base_Ant', u'FkLine__dpAR_1:Guide_JointEnd', u'FkLine__dpAR_1:Guide_JointLoc3', u'FkLine__dpAR_1:Guide_JointLoc2', u'FkLine__dpAR_1:Guide_JointLoc1']
-
-# print(dir(autoRigUI))
-# dir(autoRig)
-autoRigUI.setupDuplicatedGuide('Head__dpAR_2:Guide_Base')
-
-{u'Head__dpAR_2:Guide_Base': {'fatherMirrorAxis': u'off', 'guideModuleNamespace': u'Head__dpAR_2', 'fatherModule': u'Spine', 'fatherCustomName': u'Spine', 'fatherNode': u'Spine__dpAR_1:Guide_JointLoc3', 'parentNode': u'Spine__dpAR_1:Guide_JointLoc3', 'guideInstance': u'dpAR_2', 'guideMirrorAxis': u'off', 'fatherMirrorName': [u'L_', u'R_'], 'childrenList': [], 'fatherGuideLoc': u'JointLoc3', 'guideCustomName': u'Head', 'fatherGuide': u'Spine__dpAR_1:Guide_Base', 'fatherInstance': u'dpAR_1', 'guideModuleName': u'Head', 'guideMirrorName': [u'L_', u'R_']}, u'Spine__dpAR_1:Guide_Base': {'fatherMirrorAxis': '', 'guideModuleNamespace': u'Spine__dpAR_1', 'fatherModule': '', 'fatherCustomName': '', 'fatherNode': '', 'parentNode': '', 'guideInstance': u'dpAR_1', 'guideMirrorAxis': u'off', 'fatherMirrorName': '', 'childrenList': [u'Head__dpAR_2:Guide_Base'], 'fatherGuideLoc': '', 'guideCustomName': u'Spine', 'fatherGuide': '', 'fatherInstance': '', 'guideModuleName': u'Spine', 'guideMirrorName': [u'L_', u'R_']}}
+myDict['Guide'+str(len(myDict))] = carroA
+myDict['Guide'+str(len(myDict))] = carroB
+myDict['Guide'+str(len(myDict))] = carroC
+# print(myDict['Guide0'].name)
+# print(myDict['Guide1'].name)
+# print(myDict['Guide2'].name)
+print(carroA in myDict)
+print(carroA in myDict.values())
+print(carroA in myList)
