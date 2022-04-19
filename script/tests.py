1a6917e6a398922d5fb3cc2acc8bb31bb8f759ef
Andre <megabrean@gmail.com>
2022-04-18 23:58:39 -0300

    Funcionou!!!

--- script/tests.py
+++ script/tests.py
@@ -1,21 +1,8 @@
-# class Car():
-#     def __init__(self, name) -> None:
-#         self.name = name
+([u'Head__dpAR_26:Guide_Base_RadiusCtrl', u'Head__dpAR_26:Guide_UpperLip', u'Head__dpAR_26:Guide_UpperHead', u'Head__dpAR_26:Guide_UpperJaw', u'Head__dpAR_26:Guide_JointEnd', u'Head__dpAR_26:Guide_Chew', u'Head__dpAR_26:Guide_LowerLip', u'Head__dpAR_26:Guide_Chin', u'Head__dpAR_26:Guide_LCornerLip', u'Head__dpAR_26:Guide_RCornerLip', u'Head__dpAR_26:Guide_Jaw', u'Head__dpAR_26:Guide_Head', u'Head__dpAR_26:Guide_Neck0'],
 
-# carroA = Car('Uno')
-# carroB = Car('Gol')
-# carroC = Car('Corsa')
-# myDict = {}
-# myList = [carroA, carroB, carroC]
+[u'Head__dpAR_2:Guide_Base_RadiusCtrl', u'Head__dpAR_2:Guide_Chin', u'Head__dpAR_2:Guide_Neck', u'Head__dpAR_2:Guide_RCornerLip', u'Head__dpAR_2:Guide_LowerLip', u'Head__dpAR_2:Guide_UpperHead', u'Head__dpAR_2:Guide_UpperLip', u'Head__dpAR_2:Guide_LCornerLip', u'Head__dpAR_2:Guide_Jaw', u'Head__dpAR_2:Guide_Head', u'Head__dpAR_2:Guide_UpperJaw', u'Head__dpAR_2:Guide_JointEnd', u'Head__dpAR_2:Guide_Chew'])
 
-# myDict['Guide'+str(len(myDict))] = carroA
-# myDict['Guide'+str(len(myDict))] = carroB
-# myDict['Guide'+str(len(myDict))] = carroC
-# print(myDict['Guide0'].name)
-# print(myDict['Guide1'].name)
-# print(myDict['Guide2'].name)
-# print(carroA in myDict)
-# print(carroA in myDict.values())
-# print(carroA in myList)
 
-myList = [0,1,2]
+([u'Spine__dpAR_27:Guide_Base_RadiusCtrl', u'Spine__dpAR_27:Guide_JointLoc2', u'Spine__dpAR_27:Guide_JointLoc2_Grp', u'Spine__dpAR_27:Guide_JointEnd', u'Spine__dpAR_27:Guide_JointLoc3', u'Spine__dpAR_27:Guide_JointLoc3_Grp', u'Spine__dpAR_27:Guide_JointLoc1'],
+
+[u'Spine__dpAR_1:Guide_Base_RadiusCtrl', u'Spine__dpAR_1:Guide_JointLoc2_Grp', u'Spine__dpAR_1:Guide_JointLoc2', u'Spine__dpAR_1:Guide_JointLoc3', u'Spine__dpAR_1:Guide_JointLoc3_Grp', u'Spine__dpAR_1:Guide_JointEnd', u'Spine__dpAR_1:Guide_JointLoc1'])
